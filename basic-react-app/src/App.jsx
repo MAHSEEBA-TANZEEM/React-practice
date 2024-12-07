6ac8f2f53ad235d1e63b9cefc46b646d1750be9f
Mahseeba Tanzeem <mahseebatanzeem777@gmail.com>
2024-12-07 22:21:17 +0530

    adding files

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+// import Title from "./Title.jsx";
+// import Description from "./Description.jsx";
+import ProductTab from "./ProductTab.jsx";
+// import MsgBox from "./MsgBox";
+
+function App() {
+ return (
+  <>
+    <h2>Blockbuster Deals | Shop now</h2>
+    <ProductTab/>;
+  </>
+ );  
+}
+
+export default App;
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,6 @@
+function Title() {
+    let msg = "hello world!";
+    return <p> message says {msg.toUpperCase()}</p>;
+};
+
+export default Title;
