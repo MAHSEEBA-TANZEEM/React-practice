7339366c9d229bf15e2baa6035c20b632c142ec1
Mahseeba Tanzeem <mahseebatanzeem777@gmail.com>
2024-12-13 10:53:34 +0530

    Initial commit

---
+++ react-state-class/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from './Button.jsx'
+import Form from './Form.jsx'
+import Counter from './Counter.jsx'
+import LikeButton from './LikeButton.jsx'
+
+function App() {
+  return (
+    <>
+      <LikeButton/>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
